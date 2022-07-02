adcc98307f002ec1f409ca14e72c322da19468a9
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-07-02 23:30:08 +0100

    Final SVM model

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,8 +26,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 
+
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 import time
 
 t0 = time.time()
@@ -39,6 +41,7 @@ t1 = time.time()
 preds = clr.predict(features_test)
 print("prediction time:", round(time.time() - t1,3), "sec")
 print("prediction accuracy:", accuracy_score(preds,labels_test))
+print(confusion_matrix(labels_test,preds))
 
 #########################################################
 
